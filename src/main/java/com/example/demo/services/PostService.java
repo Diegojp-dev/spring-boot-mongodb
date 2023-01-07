612a30bbf919a33682b15605d8ad71f621a232f1
diego <diego.pavan05@gmail.com>
2023-01-07 05:43:04 -0300

    created entity Post

---
+++ src/main/java/com/example/demo/services/PostService.java
@@ -0,0 +1,50 @@
+package com.example.demo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.domain.Post;
+import com.example.demo.repository.PostRepository;
+import com.example.demo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repository;
+	
+	public List<Post> findAll(){
+	 return	repository.findAll();
+	}
+	
+	public Post findById(String id) {
+	  Post post = repository.findById(id).orElseThrow(()-> new ObjectNotFoundException(" object not found"));
+	  return post;
+	}
+	
+	public Post insert(Post post) {
+		post = repository.save(post);
+		return post;
+	}
+	
+	public void delete(String id) {
+		findById(id);
+		repository.deleteById(id);
+	}
+	
+	public Post update (String id , Post post) {
+		Post newPost = repository.findById(id).get();
+		updateData(newPost , post);
+		return newPost;
+	}
+	
+	private void updateData(Post newPost, Post post) {
+		newPost.setId(post.getId());
+		newPost.setTitle(post.getTitle());
+		newPost.setData(post.getData());
+		newPost.setBody(post.getBody());
+	}
+	
+}
